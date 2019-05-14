9049f310650caeb230afa8760dcbbfb262be9f45
Nicholas Ngai <ngai.nicholas@gmail.com>
2019-05-14 13:32:22 -0700

    Use /dev/urandom for generating random MIME boundary

--- gpgit.sh
+++ gpgit.sh
@@ -34,7 +34,7 @@ fi
 
 # Generate random MIME boundary
 mime_boundary="pgp-"
-mime_boundary+=$(dd if=/dev/random bs=32 count=1 2> /dev/null | xxd -p | tr -d '\n')
+mime_boundary+=$(dd if=/dev/urandom bs=32 count=1 2> /dev/null | xxd -p | tr -d '\n')
 
 # Rewrite headers to fit PGP/MIME, converting CRLF to LF for compatability with sed
 data_with_headers=$(echo "${data_plain}" | sed '
